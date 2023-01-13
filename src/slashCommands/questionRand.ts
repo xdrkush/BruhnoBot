61e9b1575bea1290543780c68c28706f0ec6e7b6
xdrkush <drkushikush@gmail.com>
2023-01-13 17:07:05 +0100

    fix: quizz init

--- src/slashCommands/questionRand.ts
+++ src/slashCommands/questionRand.ts
@@ -1,18 +1,263 @@
 import { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } from "discord.js"
 import { SlashCommand } from "../types";
+import { OpenAIClient } from "../client/OpenAI";
+const wait = require('node:timers/promises').setTimeout;
 
 const questions = [
     {
-        id: 1, name: "Postman", category: "back", question: "À quoi sert l'outil postman ?",
-        reponse: "C'est un outils de requetage HTTP, \nexemple: il pourrait nous être utile pour tester les routes sur une API."
+        id: 1, name: "Postman", category: "back", question: "À quoi sert l'outil postman ?", reponse: ""
     },
     {
-        id: 2, name: "Fetch", category: "front", question: "À quoi sert fetch ?",
-        reponse: "C'est une fonction devenu native en Javascript, afin d'effectuer une recherche de document, par le biais de requete http, ... il fait suite au module XMLHTTPREQUEST."
+        id: 2, name: "Fetch", category: "front", question: "À quoi sert fetch ?", reponse: ""
     },
     {
-        id: 3, name: "Sitemap", category: "uml", question: "À quoi sert un sitemap ?",
-        reponse: "Un sitemap est un plan/structure/architecture de notre siteweb effectuer via un schéma, afin d'identifier les pages, leurs enfants, les realtions et interactions entre elles."
+        id: 3, name: "Sitemap", category: "uml", question: "À quoi sert un sitemap ?", reponse: ""
+    },
+    {
+        id: 4, name: "Kamban", category: "uml", question: "C'est quoi un kanban ?", response: ""
+    },
+    {
+        id: 5, name: "Serveur", category: "system", question: "VPS vs Dedié ? Que veux dire VPS ?", response: ""
+    },
+    {
+        id: 6, name: "", category: "back", question: "Citez 1 librairie + 1 module", response: ""
+    },
+    {
+        id: 7, name: "Templating", category: "front", question: "Second layout / Helper", response: ""
+    },
+    {
+        id: 8, name: "Middleware", category: "back", question: "Qu'est-ce que sont les middlewares ?", response: ""
+    },
+    {
+        id: 9, name: "SQL", category: "database", question: "Que permet de faire le INNER JOIN ?", response: ""
+    },
+    {
+        id: 10, name: "SQL", category: "database", question: "Fais une requête SQL pour crée un article", response: ""
+    },
+    {
+        id: 11, name: "Middleware", category: "back", question: "Multer c'est quoi ? et a quoi sert-il ?", response: ""
+    },
+    {
+        id: 12, name: "Documentation", category: "back", question: "Es-ce que la doc de express est disponible en Français ?", response: ""
+    },
+    {
+        id: 13, name: "Linux", category: "system", question: "En linux a quoi servent les commandes : ls ? cp ? mkdir ?", response: ""
+    },
+    {
+        id: 14, name: "Cron", category: "system", question: "C'est quoi une tache cron ?", response: ""
+    },
+    {
+        id: 15, name: "HTTP CODE", category: "back", question: "C'est quoi l'erreur 401 ?", response: ""
+    },
+    {
+        id: 16, name: "Node", category: "back", question: "Cite 1 ou plusieurs framework pour NodeJS", response: ""
+    },
+    {
+        id: 17, name: "API", category: "back", question: "Peut-on envoyer des données dans le corp d'une requête GET ?\nSinon comment en envoyée ?", response: ""
+    },
+    {
+        id: 18, name: "Linux / Serveur", category: "system", question: "Que veux dire SSH et a quoi sert-il ?", response: ""
+    },
+    {
+        id: 19, name: "Axios", category: "back", question: "Qu'est-ce que Axios ?", response: ""
+    },
+    {
+        id: 20, name: "XSS", category: "back", question: "C'est quoi une XSS ?", response: ""
+    },
+    {
+        id: 21, name: "API", category: "back", question: "C'est quoi une API ? Comment on l'utilise ?", response: ""
+    },
+    {
+        id: 22, name: "CORS", category: "back", question: "Qu'est-ce que CORS ?", response: ""
+    },
+    {
+        id: 23, name: "Templating", category: "front", question: "C'est quoi un partial ?", response: ""
+    },
+    {
+        id: 24, name: "Module", category: "back", question: "A quoi sert un node module ? Comment on l'utilise ? Et avec quoi s'installe t-il ?", response: ""
+    },
+    {
+        id: 25, name: "Gestion de Projet", category: "uml", question: "C'est quoi un projet prédictif ?", response: ""
+    },
+    {
+        id: 26, name: "Gestion de Projet", category: "uml", question: "Qu'est-ce qu'un Persona ?", response: ""
+    },
+    {
+        id: 27, name: "Gestion de Projet", category: "uml", question: "C'est quoi un product backlog ?", response: ""
+    },
+    {
+        id: 28, name: "Gestion de Projet", category: "uml", question: "C'est quoi la méthode Hybride ? En existe-il d'autre ?", response: ""
+    },
+    {
+        id: 29, name: "Gestion de Projet", category: "uml", question: "C'est quoi le RACI ?", response: ""
+    },
+    {
+        id: 30, name: "Gestion de Projet", category: "uml", question: "Que signifie PARA ?", response: ""
+    },
+    {
+        id: 31, name: "Gestion de Projet", category: "uml", question: "Différence entre le PO et le Scrum master ?", response: ""
+    },
+    {
+        id: 32, name: "Gestion de Projet", category: "uml", question: "C'est quoi un Jalon ?", response: ""
+    },
+    {
+        id: 33, name: "Gestion de Projet", category: "uml", question: "C'est quoi un objectif SMART ?", response: ""
+    },
+    {
+        id: 34, name: "Gestion de Projet", category: "uml", question: "Qu'est-ce que le MVP ?", response: ""
+    },
+    {
+        id: 35, name: "Gestion de Projet", category: "uml", question: "Qui est le Product Owner ?", response: ""
+    },
+    {
+        id: 36, name: "Gestion de Projet", category: "uml", question: "C'est quoi des user story ?", response: ""
+    },
+    {
+        id: 37, name: "Gestion de Projet", category: "uml", question: "Différence en le POC et le MVP", response: ""
+    },
+    {
+        id: 38, name: "Gestion de Projet", category: "uml", question: "Quel est le role du Srum master ?", response: ""
+    },
+    {
+        id: 39, name: "Gestion de Projet", category: "uml", question: "Quel est le role du Product Owner ?", response: ""
+    },
+    {
+        id: 40, name: "Multer", category: "back", question: "Que représente req.file.filename avec une image ?", response: ""
+    },
+    {
+        id: 41, name: "SGBD", category: "database", question: "C'est quoi un SGBD ?", response: ""
+    },
+    {
+        id: 42, name: "JS DOM", category: "front", question: "Le JS client est présent dans : un site static ? un site dynamique ?", response: ""
+    },
+    {
+        id: 43, name: "SQL", category: "database", question: "A quoi sert Mysql Dump ?", response: ""
+    },
+    {
+        id: 44, name: "Expres-Session", category: "back", question: "Comment editer les infos stocker en session ? (req.session)", response: ""
+    },
+    {
+        id: 45, name: "Backup", category: "system", question: "C'est quoi un backup ?", response: ""
+    },
+    {
+        id: 46, name: "Proxy", category: "system", question: "Nginx c'est quoi ?", response: ""
+    },
+    {
+        id: 47, name: "TP", category: "uml", question: "Cite 5 mots clef en rapport avec le TP 6 (accès aux données)", response: ""
+    },
+    {
+        id: 48, name: "Client web", category: "front", question: "Sur quoi se base la session pour reconnaitre un client ?", response: ""
+    },
+    {
+        id: 49, name: "", category: "back", question: "Sync vs Async ?", response: ""
+    },
+    {
+        id: 50, name: "CMS", category: "system", question: "C'est quoi un CMS et a quoi sert-il ?", response: ""
+    },
+    {
+        id: 51, name: "SASS", category: "front", question: "C'est quoi SASS ? (ce n'est pas un moteur de templating !!)", response: ""
+    },
+    {
+        id: 52, name: "JS", category: "back", question: "Depuis quel ECMAScript doit-on utilisé \"import\" au lieu de déclarer une variable pour les dépendances ?", response: ""
+    },
+    {
+        id: 53, name: "", category: "back", question: "Test unitaire VS Test d'implémentation", response: ""
+    },
+    {
+        id: 54, name: "CRUD", category: "back", question: "C'est quoi un CRUD ?", response: ""
+    },
+    {
+        id: 55, name: "MVC", category: "uml", question: "C'est quoi un MVC ?", response: ""
+    },
+    {
+        id: 56, name: "Express", category: "back", question: "Comment puis-je faire suivre des data de mon middleware vers mon controllers ?", response: ""
+    },
+    {
+        id: 57, name: "Sécurité", category: "system", question: "Cite moi 2 outils pour faire de la sécurité web", response: ""
+    },
+    {
+        id: 58, name: "TP", category: "uml", question: "Cite 5 mots clef en rapport avec le TP3 (web dynamique)", response: ""
+    },
+    {
+        id: 59, name: "TP", category: "uml", question: "Cite 5 mots clef en rapport avec le TP8 (gestion de contenu Back)", response: ""
+    },
+    {
+        id: 60, name: "Sécurité", category: "system", question: "C'est quoi un certificat SSL / TLS ?", response: ""
+    },
+    {
+        id: 61, name: "HTML", category: "front", question: "Comment empêcher la saisie dans un input en html ?", response: ""
+    },
+    {
+        id: 62, name: "SEO", category: "front", question: "Comment vérifier le SEO de son site ?", response: ""
+    },
+    {
+        id: 63, name: "Sécurité", category: "back", question: "A quoi sert le dotenv ?", response: ""
+    },
+    {
+        id: 64, name: "HTTP", category: "back", question: "De quoi est constitué une requête HTTP ?", response: ""
+    },
+    {
+        id: 65, name: "Gestion de Projet", category: "uml", question: "Explique moi la méthode en V ?", response: ""
+    },
+    {
+        id: 66, name: "Linux / Serveur", category: "system", question: "Quel est la différence entre SSH et SCP ?", response: ""
+    },
+    {
+        id: 67, name: "TP", category: "uml", question: "Cite 5 mots clef en rapport avec le TP1 (maquettage)", response: ""
+    },
+    {
+        id: 68, name: "MCD/MLD", category: "uml", question: "Que signifie MCD & MLD ? De quel méthode viennent-il ?", response: ""
+    },
+    {
+        id: 69, name: "SMTP", category: "back", question: "Que veux dire SMTP ? Dans quel condition tu l'utilise ?", response: ""
+    },
+    {
+        id: 70, name: "HTML", category: "front", question: "A quoi sert multipart/formdata ?", response: ""
+    },
+    {
+        id: 71, name: "JS", category: "uml", question: "Comment se nomme l'extension des fichiers text Javascript ?", response: ""
+    },
+    {
+        id: 72, name: "Templating", category: "front", question: "Cite moi 3 moteur de templating", response: ""
+    },
+    {
+        id: 73, name: "HTTP", category: "uml", question: "Cite les 4 différentes type de requête HTTP utiliser ?", response: ""
+    },
+    {
+        id: 74, name: "Express JS", category: "back", question: "Quel est la différence entre res.render, res.send, res.end, res.json ?", response: ""
+    },
+    {
+        id: 75, name: "TP", category: "uml", question: "Cite 5 mots clef en rapport avec le TP5 (crée une BDD)", response: ""
+    },
+    {
+        id: 76, name: "TP", category: "uml", question: "Cite 5 mots clef en rapport avec le TP4 (gestion de contenu Front)", response: ""
+    },
+    {
+        id: 77, name: "NodeJS", category: "back", question: "Comment nodeJS stocke les data dans la db ? Es-ce que Node intéragit dans la DB ? Si oui comment ?", response: ""
+    },
+    {
+        id: 78, name: "React", category: "front", question: "A quoi sert useState exactement ?", response: ""
+    },
+    {
+        id: 79, name: "", category: "back", question: "Quel est la différence entre une librairie et un module ?", response: ""
+    },
+    {
+        id: 80, name: "Express", category: "back", question: "Quel est la différence majeur entre REQ et RES ?", response: ""
+    },
+    {
+        id: 81, name: "Uml", category: "uml", question: "C'est quoi UML ? Que contient-il ?", response: ""
+    },
+    {
+        id: 82, name: "SQL", category: "database", question: "Fais une jointure entre la table USERS et COMMENTS via son ID", response: ""
+    },
+    {
+        id: 83, name: "TP", category: "uml", question: "Cite 5 mots clef en rapport avec un Site static et adaptable", response: ""
+    },
+    {
+        id: 84, name: "URL", category: "back", question: "Quel est la différence entre params et query ?", response: ""
+    },
+    {
+        id: 85, name: "TP", category: "uml", question: "Cite 5 mot clef en rapport avec le TP7 (config back-end)", response: ""
     }
 ]
 
@@ -28,6 +273,8 @@ const command: SlashCommand = {
                     { name: 'Random', value: 'random' },
                     { name: 'Front', value: 'front' },
                     { name: 'Back', value: 'back' },
+                    { name: 'System', value: 'system' },
+                    { name: 'Database', value: 'database' },
                     { name: 'UML', value: 'uml' },
                 )),
     execute: async interaction => {
@@ -56,29 +303,50 @@ const command: SlashCommand = {
             embeds: [
                 new EmbedBuilder()
                     .setAuthor({ name: `📡 Category: ${question.category}` })
-                    .setDescription(`❓ Question: ${question.question}`)
+                    .setDescription(`❓ __**Question**__: ${question.question}`)
+                    .setTimestamp()
             ],
             components: [
                 row
             ]
         })
     },
-    btn: (interaction: any) => {
+    async btn(interaction: any) {
+        // try {
         const [customId, userId] = interaction.customId.split('-')
         const [getQuestion]: any = questions.filter(el => el.id.toString() === customId)
+        const gptResponse = await new OpenAIClient().searchByString(getQuestion.question)
 
-        if (interaction.user.id === userId)
-            interaction.reply({
+        console.log('reponse button', gptResponse.data)
+
+        if (interaction.user.id === userId) {
+            await interaction.reply('wait ...')
+            // await interaction.deferReply()
+            
+            const message = new EmbedBuilder()
+                .setAuthor({ name: `${interaction.user.tag}` })
+                .setDescription(`❓ __**Réponse**__: ${gptResponse.data.choices[0].text?.toString()}`)
+                .setFooter({ text: "La réponse est généré par openAI." })
+                .setTimestamp();
+
+            // await wait(2000);
+            await interaction.editReply({
                 embeds: [
-                    new EmbedBuilder()
-                        .setAuthor({ name: `${interaction.user.tag}` })
-                        .setDescription(`❓ Réponse: ${getQuestion.reponse}`)
+                    message
                 ]
             })
-        else interaction.reply({
+        }
+        else await interaction.reply({
             content: "Vous ne pouvez pas faire cette interaction !",
             ephemeral: true
         });
+
+        // } catch (error) {
+        //     interaction.reply({
+        //         content: "Une erreur est survenue !",
+        //         ephemeral: true
+        //     });
+        // }
     },
     cooldown: 10,
 }
